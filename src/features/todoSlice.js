5977b40b4e34072729048212e792961124a51f65
jwong65 <jason.wong6507@gmail.com>
2023-02-09 18:23:47 -0500

    Added the todoSlice.js feature file

---
+++ src/features/todoSlice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState ={
+    items:[]
+}
+export default function todoSlice() {
+  return (
+    <div>todoSlice</div>
+  )
+}
